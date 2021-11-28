b69011790d11219f25b08602f04a089255007c27
Filipedss123 <lipi_domingues@hotmail.com>
2021-11-28 20:13:04 -0300

    ADD VAR LOCAL

--- site do projeto/conection.php
+++ site do projeto/conection.php
@@ -1,5 +1,13 @@
 <?php
 
+// PARA CONECTAR AO BANCO DE DADOS LOCAL
+// $db_name = "test";
+// $db_host = "localhost";
+// $db_user = "root";
+// $db_password = "";
+
+//PARA CONECTAR AO BANCO DE DADOS AZURE
+
 $db_name = "contato";
 $db_host = "lbl-db-azureserver.mysql.database.azure.com";
 $db_user = "administrador";
